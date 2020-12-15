70a28a4e6cc1f02bb1ed08cf7c6338b30ce7d9e4
Muhammad Bilal <61741931+Muhammad-Bilal90@users.noreply.github.com>
2020-12-15 20:27:26 +0500

    Updated display message at src/App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World.</h1>
-      <h1>I am Muhammad Bilal and I am loving deploying my wesite on SURGE.</h1>
+      <h1>I am Muhammad Bilal and I am loving deploying my website on SURGE.</h1>
       <h1>REACT APPLICAION.</h1>
     </div>
   );
